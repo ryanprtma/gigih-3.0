10124fa7060f84217095a7b42cf37ae34d46c19a
ryan apratama <ryanapratama16@gmail.com>
2023-07-17 14:35:11 +0700

    Modul 2.1

---
+++ Fullstack Module/Modul 2.1/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello WOrld!');
+});
+
+app.listen(port, () => {
+    console.log(`example app listening on pot ${port}`);
+});
