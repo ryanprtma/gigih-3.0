0f0f495d97ab5302c21c9941baab5949ed2b9222
ryan apratama <ryanapratama16@gmail.com>
2023-08-07 12:04:29 +0700

    using conditional rendering

--- Fullstack Module/Modul 4.2/spotify-clone/src/components/RandomPlaylists.jsx
+++ Fullstack Module/Modul 4.2/spotify-clone/src/components/RandomPlaylists.jsx
@@ -5,13 +5,32 @@ import MediaControlCard from './MediaControlCard';
 export const RandomPlaylists = (props) => {
     const data = props.data;
 
+    const getGreetingBasedOnTime = () => {
+        const currentTime = new Date();
+        const currentHour = currentTime.getHours();
+
+        let greeting;
+
+        if (currentHour >= 5 && currentHour < 12) {
+            greeting = "Good morning";
+        } else if (currentHour >= 12 && currentHour < 18) {
+            greeting = "Good afternoon";
+        } else if (currentHour >= 18 && currentHour < 22) {
+            greeting = "Good evening";
+        } else {
+            greeting = "Good night";
+        }
+
+        return greeting;
+    }
+
     return (
         <>
             <Box sx={{ paddingTop: 2, paddingRight: 4, paddingLeft: 4, paddingBottom: 4, backgroundColor: 'hsl(0deg 0% 7.06%)' }}>
                 <Box sx={{ paddingTop: 0, paddingRight: 2, paddingLeft: 0, paddingBottom: 2, display: 'flex' }}>
                     <Typography variant='h3' color='white' sx={{ justifyContent: 'flex-start' }}>
                         <strong>
-                            Good Morning
+                            {getGreetingBasedOnTime()}
                         </strong>
                     </Typography>
                 </Box>
